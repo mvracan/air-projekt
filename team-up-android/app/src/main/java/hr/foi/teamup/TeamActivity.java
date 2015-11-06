0081c18a0f84ec1ab0d3a8eb9889bdf0229e0dc9
mvracan <mvracan@foi.hr>
2015-11-06 21:38:14 +0100

    kreiran xml za navigation drawer

--- team-up-android/app/src/main/java/hr/foi/teamup/TeamActivity.java
+++ team-up-android/app/src/main/java/hr/foi/teamup/TeamActivity.java
@@ -8,6 +8,6 @@ public class TeamActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_list);
+        setContentView(R.layout.activity_team);
     }
 }
