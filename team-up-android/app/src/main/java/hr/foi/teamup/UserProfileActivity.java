326dfbb865de217c0afcd717c36f5371adeecaaa
puding93 <lpalaic@foi.hr>
2015-11-06 21:39:00 +0100

    initiated activity for user profile

--- team-up-android/app/src/main/java/hr/foi/teamup/UserProfileActivity.java
+++ team-up-android/app/src/main/java/hr/foi/teamup/UserProfileActivity.java
@@ -1,7 +1,7 @@
 package hr.foi.teamup;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,9 +17,6 @@ import hr.foi.teamup.webservice.ServiceParams;
 import hr.foi.teamup.webservice.ServiceResponse;
 import hr.foi.teamup.webservice.ServiceResponseHandler;
 
-/**
- * Created by paz on 06.11.15..
- */
 public class UserProfileActivity extends AppCompatActivity {
 
     Button change;
@@ -27,7 +24,6 @@ public class UserProfileActivity extends AppCompatActivity {
     EditText lastName;
     EditText password;
     EditText confirmPassword;
-    ServiceParams updateParams;
     Person user;
 
     @Override
@@ -43,15 +39,13 @@ public class UserProfileActivity extends AppCompatActivity {
         change = (Button) findViewById(R.id.submitButton);
         change.setOnClickListener(onChange);
 
-
-
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     View.OnClickListener onChange = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Log.i("hr.foi.teamup.debug", "RegistrationActivity -- initiated user registration");
+            Log.i("hr.foi.teamup.debug", "UserProfileActivity -- initiated user update");
 
             String firstNameValue = firstName.getText().toString();
             String lastNameValue = lastName.getText().toString();
@@ -59,16 +53,17 @@ public class UserProfileActivity extends AppCompatActivity {
             String confirmPasswordValue = confirmPassword.getText().toString();
 
             if(checkValues(firstNameValue,lastNameValue,passwordValue,confirmPasswordValue)){
-                Log.i("hr.foi.teamup.debug", "RegistrationActivity -- creating new user and sending info to service");
-                user= SessionManager.getInstance(getApplicationContext()).retrieveSession("person");
+                Log.i("hr.foi.teamup.debug", "UserProfileActivity -- fetching user from session");
+                user= SessionManager.getInstance(getApplicationContext()).retrieveSession("person", Person.class);
+                Log.i("hr.foi.teamup.debug", "UserProfileActivity --  user fetched from session " + user.toString());
                 user.setName(firstNameValue);
                 user.setSurname(lastNameValue);
                 Credentials changedPassword=new Credentials(user.getCredentials().getUsername(),passwordValue);
                 user.setCredentials(changedPassword);
-                updateParams.setObject(user); // credentials to send later
-                // TODO: change url
-                new ServiceAsyncTask().execute(new ServiceParams(""+user.getidPerson(),
-                        "PUT", user, registrationHandler));
+
+                Log.i("hr.foi.teamup.debug", "UserProfileActivity --  calling web service ");
+                new ServiceAsyncTask().execute(new ServiceParams("/person/"+user.getidPerson(),
+                        "PUT", user, updateHandler));
             }
         }
     };
@@ -76,52 +71,52 @@ public class UserProfileActivity extends AppCompatActivity {
     private boolean checkValues(String firstNameValue, String lastNameValue,
                                 String passwordValue, String confirmPasswordValue){
         if(firstNameValue.length() < 3 || firstNameValue.length() > 45){
-            Log.w("hr.foi.teamup.debug","RegistrationActivity -- first name too short or too long");
+            Log.w("hr.foi.teamup.debug","UserProfileActivity -- first name too short or too long");
             firstName.setError("First name has to be 3-45 characters long");
             return false;
         }
         else if (lastNameValue.length() < 3 || lastNameValue.length() > 45){
-            Log.w("hr.foi.teamup.debug","RegistrationActivity -- last name too short or too long");
+            Log.w("hr.foi.teamup.debug","UserProfileActivity -- last name too short or too long");
             lastName.setError("Last name has to be 3-45 characters long");
             return false;
         }
 
         else if(passwordValue.length() < 5){
-            Log.w("hr.foi.teamup.debug","RegistrationActivity -- password too short");
+            Log.w("hr.foi.teamup.debug","UserProfileActivity-- password too short");
             password.setError("Password too short (min 5)");
             return false;
         }
         else if(!confirmPasswordValue.equals(passwordValue)){
-            Log.w("hr.foi.teamup.debug","RegistrationActivity -- passwords do not match");
+            Log.w("hr.foi.teamup.debug","UserProfileActivity -- passwords do not match");
             confirmPassword.setError("Passwords do not match");
             return false;
         }
-        Log.i("hr.foi.teamup.debug","RegistrationActivity -- all fields are valid");
+        Log.i("hr.foi.teamup.debug", "UserProfileActivity -- all fields are valid");
         return true;
     }
 
-    ServiceResponseHandler registrationHandler = new ServiceResponseHandler() {
+    ServiceResponseHandler updateHandler = new ServiceResponseHandler() {
         @Override
         public boolean handleResponse(ServiceResponse response) {
             if(response.getHttpCode() == 200) {
-                Log.i("hr.foi.teamup.debug", "RegistrationActivity -- successfully registered user, logging in now...");
-                // login
-
+                Log.i("hr.foi.teamup.debug", "UserProfileActivity -- successfully updated user");
+                SessionManager manager= SessionManager.getInstance(getApplicationContext());
+                manager.destroySession("person");
+                manager.createSession(user, "person");
+                Toast.makeText(getApplicationContext(),
+                        "Update successful", Toast.LENGTH_LONG).show();
+                finish();
                 return true;
             } else {
                 Log.w("hr.foi.teamup.debug",
-                        "RegistrationActivity -- registration failed, server returned code " + response.getHttpCode());
+                        "UserProfileActivity -- update failed, server returned code " + response.getHttpCode());
                 // show fail
                 Toast.makeText(getApplicationContext(),
-                        "Registration failed, please try again (" + response.getHttpCode() + ")",
+                        "Update failed, please try again ("+response.getHttpCode()+")",
                         Toast.LENGTH_LONG).show();
                 return false;
             }
         }
     };
-
-
-
-
 
 }
